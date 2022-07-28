a5021e08028091066ea10386d2fcb744f09abfca
Vilém Jeniš <vilemj@kentico.com>
2022-07-28 16:13:25 +0200

    Alter the merge-conflict kata to suit our needs.

--- merge-conflict/README.md
+++ merge-conflict/README.md
@@ -10,11 +10,25 @@ In this kata git cannot figure out how to merge the content added on `merge-conf
 
 Both changes need to be in master when you're done.
 
+1. Which branches exist?
+    1. `tig --all` Remember?
+    2. Does it remind you of something?
 1. Use `git merge` to bring the changes from `merge-conflict-branch1` on to `master`.
 2. What does `git status` now report.
 3. Fix the conflict with your favorite editor.
 4. Follow the instructions in `git status` to complete the merge.
-5. What does `git log --oneline --graph` show?
+5. What does `tig --all` show?
+   1. Look at the merge commit.
+   2. Who's the author of the merge commit?
+   3. Did you really make all the changes in that commit?
+
+      (Imagine someone else did the changes in master.)
+6. Let's get back to the start:
+   1. `pushd .. && yes | rm -r exercise && ./setup.sh && popd`
+2. Let's try to put our changes (`merge-conflict-branch1`) to master a different (better) way.
+   1. Let's make it linear like we did with `pull --rebase`
+   2. Instead of merging _that branch_ into master with a merge commit, we'll need to merge it using a fast-forward strategy. And in order to do taht we first need to redo all our changes on top of the latest commit in master.
+3. What do I need to do before I use `git rebase`?
 
 ## Relevant commands
 - `git merge`
--- merge-conflict/setup.sh
+++ merge-conflict/setup.sh
@@ -6,21 +6,50 @@ source ../utils/utils.sh
 kata="merge-conflict"
 make-exercise-repo
 
+# Use someone else's identity
+git config --local --add user.name "Someone Else"
+
 touch greeting.txt
 git add greeting.txt
 git commit -m "Add file greeting.txt"
-echo "hello" > greeting.txt
+echo "hello" >greeting.txt
 git add greeting.txt
 git commit -m "Add content to greeting.txt"
+cat ../colors.js >colors.js
+git add colors.js
+git commit -m "Add colors."
 
-# Create a file on branch1
+# Make modification on branch 1
 git checkout -b $kata-branch1
-echo "This is a relevant fact" > file.txt
-git add file.txt
-git commit -m "add relevant fact"
 
+# Use correct identity
+git config --local --unset user.name
 
+sed -i.orig -E "s/2E0900/2E0900',\n    KenticoOrangeOriginal: '#F05A22/" colors.js
+rm *orig
+git add colors.js
+git commit -m "Add KenticoOrangeOriginal Color"
+
+sed -i.orig -E "s/001822/001822',\n    SymbioBlue: '#0093ff/" colors.js
+rm *orig
+git add colors.js
+git commit -m "Add SymbioBlue Color"
+
+# Make modifications on master
 git checkout master
-echo "This is an indispensable truth!" > file.txt
-git add file.txt
-git commit -m "add indispensable truth!"
+
+# Use someone else's identity
+git config --local --add user.name "Someone Else"
+
+sed -i.orig -E "s/2E0900/2E0900',\n    KenticoOrange55: '#DB3C00/" colors.js
+rm *orig
+git add colors.js
+git commit -m "Add KenticoOrange55 Color"
+
+sed -i.orig -E "s/001822/001822',\n    Transparent: 'transparent/" colors.js
+rm *orig
+git add colors.js
+git commit -m "Add Transparent Color"
+
+# Revert back to correct identity
+git config --local --unset user.name
