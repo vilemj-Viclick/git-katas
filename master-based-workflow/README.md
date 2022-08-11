9c3b5833b827a2b7dc03f10ae6aacde8477fd965
Vilém Jeniš <vilemj@kentico.com>
2022-08-11 13:54:09 +0200

    Remove all PS1 scripts. Improve Vilem'S kata's scripts and readmes.

--- master-based-workflow/README.md
+++ master-based-workflow/README.md
@@ -16,12 +16,12 @@ Run `source setup.sh` (or `.\setup.ps1` in PowerShell) to setup the exercise.
 2. Change to the local repository with the command `cd local-repo`
 3. Add a line of text to `README.md`
 4. Commit the change 
-5. Run `tig` and notice how your local master branch relates to the remote master branch
+5. Run `tig --all` and notice how your local master branch relates to the remote master branch
 6. Push the change to the remote using the command `git push`
-7. Run `tig` to see that you are up-to-date
+7. Run `tig --all` to see that you are up-to-date
 8. Add another line of text to `README.md`
 9. Commit the change
-10. Run the command `../fitzgerald-pushes-before-we-do.sh` (or `..\fitzgerald-pushes-before-we-do.ps1` in PowerShell) to simulate a collaborator delivering changes to the fake remote
+10. Run the command `../fitzgerald-pushes-before-we-do.sh` to simulate a collaborator delivering changes to the fake remote
     1. We will want to show here, that a change has been made to the remote, but the local repo has no idea. No remote tracking branch update. Nothing.
 11. Push your change. Notice that they are rejected by the remote
 12. Run the command `git fetch` to retrieve the changes from the fake remote
--- merge-conflict/setup.sh
+++ merge-conflict/setup.sh
@@ -6,24 +6,18 @@ source ../utils/utils.sh
 kata="merge-conflict"
 make-exercise-repo
 
-# Use someone else's identity
-git config --local --add user.name "Someone Else"
-
 touch greeting.txt
 git add greeting.txt
-git commit -m "Add file greeting.txt"
+git commit -m "Add file greeting.txt" --author "Someone Else <someone@else.com>"
 echo "hello" >greeting.txt
 git add greeting.txt
-git commit -m "Add content to greeting.txt"
+git commit -m "Add content to greeting.txt" --author "Someone Else <someone@else.com>"
 cat ../colors.js >colors.js
 git add colors.js
-git commit -m "Add colors."
+git commit -m "Add colors." --author "Someone Else <someone@else.com>"
 
 # Make modification on branch 1
 git checkout -b $kata-branch1
-
-# Use correct identity
-git config --local --unset user.name
 
 sed -i.orig -E "s/2E0900/2E0900',\n    KenticoOrangeOriginal: '#F05A22/" colors.js
 rm *orig
@@ -38,18 +32,12 @@ git commit -m "Add SymbioBlue Color"
 # Make modifications on master
 git checkout master
 
-# Use someone else's identity
-git config --local --add user.name "Someone Else"
-
 sed -i.orig -E "s/2E0900/2E0900',\n    KenticoOrange55: '#DB3C00/" colors.js
 rm *orig
 git add colors.js
-git commit -m "Add KenticoOrange55 Color"
+git commit -m "Add KenticoOrange55 Color" --author "Someone Else <someone@else.com>"
 
 sed -i.orig -E "s/001822/001822',\n    Transparent: 'transparent/" colors.js
 rm *orig
 git add colors.js
-git commit -m "Add Transparent Color"
-
-# Revert back to correct identity
-git config --local --unset user.name
+git commit -m "Add Transparent Color" --author "Someone Else <someone@else.com>"
